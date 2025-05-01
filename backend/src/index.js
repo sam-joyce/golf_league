3625d35243e8e8f02a22afcab8ce56f768b7df03
Sam <samjoyce18@yahoo.com>
2025-05-01 19:49:21 +0100

    feat: nev file added and port configured

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server running on port 3000");
 })
