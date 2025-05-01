7e9ad9bd9b36a2fc35ee8225476ad9cca7b8d8a8
Sam <samjoyce18@yahoo.com>
2025-05-01 21:15:01 +0100

    feat: register and login routes setup and tested locally

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config";
 
+import authRoutes from "./routes/authRoutes.js";
+
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server runnning on port ${PORT}`);
 })
